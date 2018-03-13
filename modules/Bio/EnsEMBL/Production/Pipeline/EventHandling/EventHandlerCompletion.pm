d352915a7e8185b37160a78082434e3183d4b078
Dan Staines <dstaines@ebi.ac.uk>
2018-03-13 17:25:32 +0000

    remember, Perl<>JSON

--- modules/Bio/EnsEMBL/Production/Pipeline/EventHandling/EventHandlerCompletion.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/EventHandling/EventHandlerCompletion.pm
@@ -1,14 +1,15 @@
-package Bio::EnsEMBL::Production::Pipeline::EventHandling::EventHandler;
+package Bio::EnsEMBL::Production::Pipeline::EventHandling::EventHandlerCompletion;
 
 use strict;
 use warnings;
+use JSON;
 
 use base qw(Bio::EnsEMBL::Production::Pipeline::Common::Base);
 
 sub run {
     my ($self) = @_;
     my $token = $self->param_required('init_job_id');
-    my $output = {"status":"success"};
+    my $output = {"status"=>"success"};
     $self->dataflow_output_id(	   
 			      {
 			       job_id => $token,
