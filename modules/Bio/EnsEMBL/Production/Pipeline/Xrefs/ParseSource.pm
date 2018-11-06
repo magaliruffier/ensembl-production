76f99d3f57ca47fe6d7ba2a51351cfe005b60759
Alessandro Vullo <avullo@ebi.ac.uk>
2018-11-06 13:43:08 +0000

    ParseSource step redefined according to new xref framework.

--- modules/Bio/EnsEMBL/Production/Pipeline/Xrefs/ParseSource.pm
+++ modules/Bio/EnsEMBL/Production/Pipeline/Xrefs/ParseSource.pm
@@ -20,6 +20,9 @@ package Bio::EnsEMBL::Production::Pipeline::Xrefs::ParseSource;
 
 use strict;
 use warnings;
+
+use Carp;
+
 use XrefParser::Database;
 use File::Basename;
 
@@ -27,57 +30,59 @@ use parent qw/Bio::EnsEMBL::Production::Pipeline::Xrefs::Base/;
 
 sub run {
   my ($self) = @_;
-  my $parser       = $self->param_required('parser');
-  my $species      = $self->param_required('species');
-  my $species_id   = $self->param_required('species_id');
-  my $file_name    = $self->param_required('file_name');
-  my $source_id    = $self->param_required('source');
-  my $xref_url     = $self->param_required('xref_url');
-  my $db           = $self->param('db');
-  my $release_file = $self->param('release_file');
 
-  $self->dbc()->disconnect_if_idle() if defined $self->dbc();
+  my $parser = $self->make_parser({ parser       => $self->param_required('parser'),
+				    species      => $self->param_required('species'),
+				    species_id   => $self->param_required('species_id'),
+				    file_name    => $self->param_required('file_name'),
+				    source_id    => $self->param_required('source'),
+				    xref_url     => $self->param_required('xref_url'),
+				    db           => $self->param('db'),
+				    release_file => $self->param('release_file') });
 
-  my ($user, $pass, $host, $port, $dbname) = $self->parse_url($xref_url);
+  eval { $parser->run(); };
+  croak sprintf("Parsing %s source failed: $@", $params->{parser}) if $@;
 
-  my $xref_dbc = XrefParser::Database->new({
-            host    => $host,
-            dbname  => $dbname,
-            port    => $port,
-            user    => $user,
-            pass    => $pass });
+  $self->cleanup_DBAdaptor($db) if defined $db;
+}
 
-  my $dbi = $self->get_dbi($host, $port, $user, $pass, $dbname);
+=head2 make_parser
 
-  my @files;
-  push @files, $file_name;
+Factory method: instantiate requested parser
 
-  my $module = "XrefParser::$parser";
+=cut
+
+sub make_parser {
+  my ($self, $params) = @_;
+
+  my $module = "XrefParser::" . $params->{parser};
   eval "require $module";
-  my $xref_run = $module->new($xref_dbc);
-  my $failure = 0;
-  if (defined $db) {
-    my $registry = 'Bio::EnsEMBL::Registry';
-    my $dba = $registry->get_DBAdaptor($species, $db);
-    $dba->dbc()->disconnect_if_idle();
-    $failure += $xref_run->run_script( { source_id  => $source_id,
-                             species_id => $species_id,
-                             dba        => $dba,
-                             rel_file   => $release_file,
-                             dbi        => $dbi,
-                             species    => $species,
-                             file       => $file_name}) ;
-    $self->cleanup_DBAdaptor($db);
-  } else {
-    $failure += $xref_run->run( { source_id  => $source_id,
-                      species_id => $species_id,
-                      species    => $species,
-                      rel_file   => $release_file,
-                      dbi        => $dbi,
-                      files      => [@files] }) ;
-  }
-  if ($failure) { die; }
+  croak "Unable to instantiate parser: $@" if $@;
 
+  my ($user, $pass, $host, $port, $dbname) = $self->parse_url($params->{xref_url});
+  my %args =
+    (
+     source_id  => $params->{source_id},
+     species_id => $params->{species_id},
+     species    => $params->{species},
+     rel_file   => $params->{release_file},
+     files      => [ $params->{file_name} ],
+     xref_dba   => Bio::EnsEMBL::Xref::DBSQL::BaseAdaptor->new({host    => $host,
+								dbname  => $dbname,
+								port    => $port,
+								user    => $user,
+								pass    => $pass }),
+     verbose    => 1
+    );
+
+  if (defined $$params->{db}) {
+    my $registry = 'Bio::EnsEMBL::Registry';
+    
+    $args{dba} = $registry->get_DBAdaptor($params->{species}, $params->{db});
+    $args{dba}->dbc->disconnect_if_idle();
+  }
+  
+  return $module->new(%args);
 }
 
 1;
